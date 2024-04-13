7aa78a4a06b0a7b7b0250b3f19dfc32f5a4c0893
Nadiia Fedii <nadiikafedii@gmail.com>
2024-04-13 20:52:59 +0300

    added code for another task of 8 level

---
+++ Fedii_level81.py
@@ -0,0 +1,16 @@
+#level 8 code, performed by the GPT chat, all tests passed the first time
+def smash(words):
+    return ' '.join(words)
+
+#level 8 code, performed by Gemini chat, all tests passed the first time
+def smash(words):
+  """Smash words together into a sentence.
+
+  Args:
+    words: A list of words.
+
+  Returns:
+    A sentence made by joining the words in the list with spaces.
+  """
+
+  return " ".join(words)

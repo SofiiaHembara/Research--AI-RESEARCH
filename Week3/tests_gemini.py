dc408757cf2ddcb4481ee72a6f6a45db058068e4
Nadiia Fedii <nadiikafedii@gmail.com>
2024-04-28 17:36:01 +0300

    останні версії

--- Week3/tests_gemini.py
+++ Week3/tests_gemini.py
@@ -2,23 +2,16 @@ import unittest
 from correct_code import caesar_encode, caesar_decode
 
 class CaesarTests(unittest.TestCase):
-  def test_encode_simple(self):
-    self.assertEqual(caesar_encode("hello", 3), "khoor")
-  
-  def test_encode_wrap(self):
-    self.assertEqual(caesar_encode("zebra", 3), "abcde")
-  
-  def test_encode_spaces(self):
-    self.assertEqual(caesar_encode("hello world", 5), "jlmmp yaxpa")
-  
-  def test_decode_simple(self):
-    self.assertEqual(caesar_decode("khoor", 3), "hello")
-  
-  def test_decode_wrap(self):
-    self.assertEqual(caesar_decode("abcde", 3), "zebra")
-  
-  def test_decode_spaces(self):
-    self.assertEqual(caesar_decode("jlmmp yaxpa", 5), "hello world")
+  TEST_DATA = [
+      ("hello", 3, "khoor"),
+      ("zebra", 3, "abcde"),
+      ("hello world", 5, "jlmmp yaxpa"),
+  ]
+
+  def test_caesar(self):
+    for message, key, expected_result in self.TEST_DATA:
+      self.assertEqual(caesar_encode(message, key), expected_result)
+      self.assertEqual(caesar_decode(message, key), expected_result[::-1])  # decode with reverse
 
 if __name__ == "__main__":
   unittest.main()
--- Week3/tests_gpt.py
+++ Week3/tests_gpt.py
@@ -3,20 +3,29 @@ from correct_code import caesar_encode, caesar_decode
 
 class TestCaesar(unittest.TestCase):
     def test_encode(self):
-        self.assertEqual(caesar_encode('computer', 3), 'frpsxwhu')
-        self.assertEqual(caesar_encode('hello world', 5), 'mjqqt btwqi')
-        self.assertEqual(caesar_encode('abc', 1), 'bcd')
-        self.assertEqual(caesar_encode('xyz', 5), 'cde')
+        test_cases = [('computer', 3, 'frpsxwhu'),
+                      ('hello world', 5, 'mjqqt btwqi'),
+                      ('abc', 1, 'bcd'),
+                      ('xyz', 5, 'cde')]
+        for message, key, expected in test_cases:
+            with self.subTest(message=message, key=key):
+                self.assertEqual(caesar_encode(message, key), expected)
 
     def test_decode(self):
-        self.assertEqual(caesar_decode('frpsxwhu', 3), 'computer')
-        self.assertEqual(caesar_decode('mjqqt btwqi', 5), 'hello world')
-        self.assertEqual(caesar_decode('bcd', 1), 'abc')
-        self.assertEqual(caesar_decode('cde', 5), 'xyz')
+        test_cases = [('frpsxwhu', 3, 'computer'),
+                      ('mjqqt btwqi', 5, 'hello world'),
+                      ('bcd', 1, 'abc'),
+                      ('cde', 5, 'xyz')]
+        for message, key, expected in test_cases:
+            with self.subTest(message=message, key=key):
+                self.assertEqual(caesar_decode(message, key), expected)
 
     def test_circular_encode_decode(self):
-        self.assertEqual(caesar_decode(caesar_encode('hello world', 5), 5), 'hello world')
-        self.assertEqual(caesar_decode(caesar_encode('abc', 1), 1), 'abc')
+        test_cases = [('hello world', 5),
+                      ('abc', 1)]
+        for message, key in test_cases:
+            with self.subTest(message=message, key=key):
+                self.assertEqual(caesar_decode(caesar_encode(message, key), key), message)
 
 if __name__ == '__main__':
     unittest.main()
